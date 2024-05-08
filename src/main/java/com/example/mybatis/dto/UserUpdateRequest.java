de247570850a095164374bfbc56952399e3c8b3a
mori.oeda <mori.oeda@fonfun.co.jp>
2024-05-08 15:27:29 +0900

    ユーザー検索・追加・編集・削除機能の追加

--- src/main/java/com/example/mybatis/dto/UserUpdateRequest.java
+++ src/main/java/com/example/mybatis/dto/UserUpdateRequest.java
@@ -1,5 +1,19 @@
 package com.example.mybatis.dto;
 
-public class UserUpdateRequest {
-
+import java.io.Serializable;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+/**
+ * ユーザー情報更新リクエストデータ
+ *
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class UserUpdateRequest extends UserAddRequest implements Serializable {
+    /**
+     * ユーザーID
+     */
+    @NotNull
+    private Long id;
 }
